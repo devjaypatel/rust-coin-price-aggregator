43f9c22e7fda5001abe770139dd95d432409ff54
Jay Patel <jaykpatel304@gmail.com>
2023-06-25 21:01:30 +0530

    fix : handing errors properly

--- src/api/controller/controller.rs
+++ src/api/controller/controller.rs
@@ -1,12 +1,8 @@
-use std::{collections::{BTreeMap,HashMap} };
+use std::collections::{BTreeMap, HashMap};
 
-use crate::models::{
-    actix,
-    coin_pairs::CoinNameQuery,
-    trade_history::{ CoinPrice},
-};
+use crate::models::{actix, coin_pairs::CoinNameQuery, trade_history::CoinPrice};
 use actix_web::{web, Error, HttpResponse, Result};
-use redis::Commands;
+use redis::{Commands, RedisError};
 
 pub async fn health_check() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().body("healthy"))
@@ -18,38 +14,18 @@ pub async fn get_coin_price_list(
 ) -> Result<HttpResponse, Error> {
     let redis_client = &connection.redis;
 
-    let mut data: Vec<HashMap<String,CoinPrice>> = Vec::new();
+    let mut data: Vec<HashMap<String, CoinPrice>> = Vec::new();
 
     if params.coin_name.is_some() {
         let coin_name: &String = &params.coin_name.as_ref().unwrap();
-        let result: String = redis_client
-            .lock()
-            .unwrap()
-            .hget("coin_price", coin_name)
-            .expect("failed to get coin price in redis");
+        let result: Result<String, RedisError> =
+            redis_client.lock().unwrap().hget("coin_price", coin_name);
 
-        match serde_json::from_str::<CoinPrice>(&result) {
-            Ok(m) => {
-                let mut hm = HashMap::new();
-                hm.insert(coin_name.into(), m);
-                data.push(hm);
-            }
-            Err(err) => {
-                println!("{:?}", err);
-            }
-        }
-    } else {
-        let result: BTreeMap<String, String> = redis_client
-            .lock()
-            .unwrap()
-            .hgetall("coin_price")
-            .expect("failed to get coin price in redis");
-
-        for (coin_name,coin_price) in result.iter() {
-            match serde_json::from_str::<CoinPrice>(&coin_price) {
+        if result.is_ok() {
+            match serde_json::from_str::<CoinPrice>(&result.unwrap()) {
                 Ok(m) => {
                     let mut hm = HashMap::new();
-                    hm.insert(coin_name.clone(), m);
+                    hm.insert(coin_name.into(), m);
                     data.push(hm);
                 }
                 Err(err) => {
@@ -57,8 +33,25 @@ pub async fn get_coin_price_list(
                 }
             }
         }
-    }
+    } else {
+        let result: Result<BTreeMap<String, String>, RedisError> =
+            redis_client.lock().unwrap().hgetall("coin_price");
 
+        if result.is_ok() {
+            for (coin_name, coin_price) in result.unwrap().iter() {
+                match serde_json::from_str::<CoinPrice>(&coin_price) {
+                    Ok(m) => {
+                        let mut hm = HashMap::new();
+                        hm.insert(coin_name.clone(), m);
+                        data.push(hm);
+                    }
+                    Err(err) => {
+                        println!("{:?}", err);
+                    }
+                }
+            }
+        }
+    }
 
     Ok(HttpResponse::Ok().json(data))
 }
